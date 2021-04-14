5afab5452c43799451171f6cced5a7b9bd981020
Mic Dom <micdom95@gmail.com>
2021-04-14 21:15:58 +0200

    Add project files.

---
+++ SquareEquationCalculator.Tests/SquareEquationCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SquareEquation
+{
+    [TestFixture]
+    [Author("michal.domaradzki@student.wsb.edu.pl")]
+    [Category("Math - Square equation")]
+    public class SquareEquationCalculatorTests
+    {
+       
+    } 
+}
---
+++ SquareEquationCalculator/SquareEquationCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareEquation
+{
+    public class SquareEquationCalculator
+    {
+        public string message;
+        public int rootCounter;
+        public double a, b, c;
+        public double delta, denominator, x1, x2;
+        public SquareEquationCalculator()
+        {
+            delta = -1;
+        }
+        public void CalculateSquareEquation(double a, double b, double c)
+        {
+            if (a != 0)
+            {
+                delta = Math.Pow(b,2) - (4 * a * c);
+                denominator = 2 * a;
+                
+                if (delta > 0)
+                {
+                    x1 = (-b / denominator) + (Math.Sqrt(delta) / denominator);
+                    x2 = (-b / denominator) - (Math.Sqrt(delta) / denominator);
+                    message = "Function with two roots";
+                    rootCounter = 2;
+                }
+                else if (delta == 0)
+                {
+                    x1 = -b / denominator;
+                    message = "Function with one root";
+                    rootCounter = 1;
+                }
+                else
+                {
+                    message = "Function without roots";
+                    rootCounter = 0;
+                }
+            }
+            else
+            {
+                message = "Exception: Divided by 0";
+                denominator = 0;
+            }
+        }
+    }
+}
