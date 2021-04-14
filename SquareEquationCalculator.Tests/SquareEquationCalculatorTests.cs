6f9f109db956d332d2151b745e5d6e7f58ff4541
Mic Dom <micdom95@gmail.com>
2021-04-14 21:17:09 +0200

    Tests with firts value equal to zero.

--- SquareEquationCalculator.Tests/SquareEquationCalculatorTests.cs
+++ SquareEquationCalculator.Tests/SquareEquationCalculatorTests.cs
@@ -11,6 +11,34 @@ namespace SquareEquation
     [Category("Math - Square equation")]
     public class SquareEquationCalculatorTests
     {
-       
+        [Test]
+        [TestCase(0, 0, 0, "Exception: Divided by 0")]
+        [TestCase(0, 1, 1, "Exception: Divided by 0")]
+        [TestCase(0, -1, -1, "Exception: Divided by 0")]
+        [TestCase(0, 1, -1, "Exception: Divided by 0")]
+        [TestCase(0, -1, 1, "Exception: Divided by 0")]
+        [Description("Test prove that if first value is equal to zero that denominator is equal to 0 and that generate exception message.")]
+        public void CalculateSquareEquation_FirstValueEqualZero_MessageAboutException(double firtsValue, double secondValue, double thirdValue, string expectedMessageText)
+        {
+            SquareEquationCalculator sec = new SquareEquationCalculator();
+            sec.CalculateSquareEquation(firtsValue, secondValue, thirdValue);
+            string resultMessage = sec.message;
+            Assert.AreEqual(expectedMessageText, resultMessage);
+        }
+
+        [Test]
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(0, 1, 1, 0)]
+        [TestCase(0, -1, -1, 0)]
+        [TestCase(0, 1, -1, 0)]
+        [TestCase(0, -1, 1, 0)]
+        [Description("Test prove that if first value is equal to zero that denominator is equal to 0 and that generate exception message.")]
+        public void CalculateSquareEquation_FirstValueEqualZero_DenominatorEqualToZero(double firtsValue, double secondValue, double thirdValue, double expectedDenominatorValue)
+        {
+            SquareEquationCalculator sec = new SquareEquationCalculator();
+            sec.CalculateSquareEquation(firtsValue, secondValue, thirdValue);
+            double resultMessage = sec.denominator;
+            Assert.AreEqual(expectedDenominatorValue, resultMessage);
+        }
     } 
 }
